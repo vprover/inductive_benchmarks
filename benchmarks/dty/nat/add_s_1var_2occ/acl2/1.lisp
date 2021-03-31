b72a0969ba2c34fa668e565f9b0630db126e2ccd
mezpusz <mezpusz@hotmail.com>
2021-03-31 09:51:07 +0200

    Remove unneeded files

--- benchmarks/dty/nat/add_s_1var_2occ/acl2/1.lisp
+++
@@ -1,6 +0,0 @@
-     (DEFUN S (X0) (LIST X0))
-     (DEFUN ADD (X0 X1)
-            (COND ((ENDP X0) X1)
-                  ((CONSP X0) (S (ADD (CAR X0) X1)))))
-     (DEFTHM THEOREM
-             (= (ADD (S X) X) (S (ADD X X)))))
