5f0521f91adb8bb95843294ff482eb9e00efd20f
mezpusz <mezpusz@hotmail.com>
2021-05-05 20:04:14 +0200

    Add easier variant of even multiplication problem

---
+++ benchmarks/dty/nat/crafted_even/acl2/2.lisp
@@ -0,0 +1,16 @@
+     (DEFUN S (X0) (LIST X0))
+     (DEFUN ADD (X0 X1)
+            (COND ((ENDP X0) X1)
+                  ((CONSP X0) (S (ADD (CAR X0) X1)))))
+     (DEFUN EVEN (X0)
+            (COND ((ENDP X0) T)
+                  ((CONSP X0) (COND
+                        ((ENDP (CAR X0)) (NOT T))
+                        ((CONSP (CAR X0))
+                              (EVEN (CAR (CAR X0))))))))
+     (DEFUN MUL (X0 X1)
+            (COND ((ENDP X0) X0)
+                  ((CONSP X0)
+                   (ADD (MUL (CAR X0) X1) X1))))
+     (DEFTHM THEOREM
+             (IMPLIES (EVEN Y) (EVEN (MUL X Y))))
